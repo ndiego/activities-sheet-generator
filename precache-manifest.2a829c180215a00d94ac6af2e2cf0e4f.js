c0499634056865bb49d2abd33fc8554a028a2868
Nick Diego <ndiego@outermostdesign.com>
2020-09-15 13:28:42 -0400

    Updates

---
+++ precache-manifest.2a829c180215a00d94ac6af2e2cf0e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c88e3fb86a57134362b2ad0900eb6f1",
+    "url": "/activities-sheet-generator/index.html"
+  },
+  {
+    "revision": "99fb8f6d2e6c1e029118",
+    "url": "/activities-sheet-generator/static/css/main.1985a3f7.chunk.css"
+  },
+  {
+    "revision": "d74126fe49b01ca6a095",
+    "url": "/activities-sheet-generator/static/js/2.4adc5f6f.chunk.js"
+  },
+  {
+    "revision": "7f740f2a47f7b1aa52894760c56f5bcb",
+    "url": "/activities-sheet-generator/static/js/2.4adc5f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fb8f6d2e6c1e029118",
+    "url": "/activities-sheet-generator/static/js/main.5a8d6da5.chunk.js"
+  },
+  {
+    "revision": "4a9b73dc255662de3e8e",
+    "url": "/activities-sheet-generator/static/js/runtime-main.9c87c9a9.js"
+  }
+]);
